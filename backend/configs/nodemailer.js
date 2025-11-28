e9edd779d3c5ed942f8af9c051e149e295f43a75
kalugithub1 <167551008+kalugithub1@users.noreply.github.com>
2025-11-28 22:05:01 +0300

    change from 587 to 465

--- backend/configs/nodemailer.js
+++ backend/configs/nodemailer.js
@@ -4,7 +4,7 @@ import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 587,
+  port: 465,
   secure: false,
   auth: {
     user: process.env.SMTP_USER,
